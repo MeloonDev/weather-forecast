9b50c11de73ff9952f59e4be82cf2df07fade041
Mateusz Melaniuk <mateusz.melaniuk98@gmail.com>
2023-06-30 20:53:48 +0200

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <main className="flex justify-center items-center bg-gradient-to-br from-sky-400 via-purple-500 to-rose-500 h-[100vh] w-full">
+      Hello World
+    </main>
+  );
+}
+
+export default App;
