3a7150053d8c1cdfa068c470d605004deabafd2a
Mateusz Melaniuk <mateusz.melaniuk98@gmail.com>
2023-07-01 14:46:41 +0200

    API call for geolocation and weather forecast

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,59 @@
-function App() {
+import { useEffect, useState } from "react";
+import { optionType } from "./types";
+
+function App(): JSX.Element {
+  const [term, setTerm] = useState<string>("");
+  const [city, setCity] = useState<optionType | null>(null);
+  const [options, setOptions] = useState<[]>([]);
+
+  const getSearchOptions = (value: string) => {
+    fetch(
+      `http://api.openweathermap.org/geo/1.0/direct?q=${value.trim()}&limit=5&appid=${
+        import.meta.env.VITE_APP_API_KEY
+      }`
+    )
+      .then((res) => res.json())
+      .then((data) => setOptions(data));
+  };
+
+  const onIputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value.trim();
+    setTerm(value);
+
+    if (value === "") return;
+
+    getSearchOptions(value);
+  };
+
+  const getForecast = (city: optionType) => {
+    fetch(
+      `https://api.openweathermap.org/data/2.5/weather?lat=${city.lat}&lon=${
+        city.lon
+      }&units=metric&appid=${import.meta.env.VITE_APP_API_KEY}`
+    )
+      .then((res) => res.json())
+      .then((data) => console.log(data));
+  };
+
+  const onSubmit = () => {
+    if (!city) return;
+
+    getForecast(city);
+  };
+
+  const onOptionSelect = (option: optionType) => {
+    setCity(option);
+  };
+
+  useEffect(() => {
+    if (city) {
+      setTerm(city.name);
+      setOptions([]);
+    }
+  }, [city]);
+
+  // https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}
+
   return (
     <main className="flex justify-center items-center bg-gradient-to-br from-sky-400 via-purple-500 to-rose-500 h-[100vh] w-full">
       <section className="w-full md:max-w-[500px] p-4 flex flex-col text-center items-center justify-center md:px-10 lg:p-24 h-full lg:h-[500px] bg-white bg-opacity-20 backdrop-blur-ls drop-shadow-lg rounded text-zinc-700 ">
@@ -8,13 +63,29 @@ function App() {
         <p className="text-sm mt-2">
           Enter below a place you want to know the weather
         </p>
-        <div className="flex mt-10 md:mt-4">
+        <div className="relative flex mt-10 md:mt-4">
           <input
             type="text"
-            value={"h"}
+            value={term}
+            onChange={onIputChange}
             className="px-2 py-1 rounded-l-md border-2 border-white focus:outline-none"
           />
-          <button className="rounded-r-md border-2 border-zinc-100 hover:border-zinc-500 hover:text-zinc-500 text-zinc-100 px-2 py-1 cursor-pointer">
+          <ul className="absolute top-9 bg-white ml-1 rounded-b-md">
+            {options.map((option: optionType, index: number) => (
+              <li key={option.name + "-" + index}>
+                <button
+                  className="text-left text-sm w-full hover:bg-zinc-700 hover:text-white px-2 py-1 cursor-pointer"
+                  onClick={() => onOptionSelect(option)}
+                >
+                  {option.name}
+                </button>
+              </li>
+            ))}
+          </ul>
+          <button
+            className="rounded-r-md border-2 border-zinc-100 hover:border-zinc-500 hover:text-zinc-500 text-zinc-100 px-2 py-1 cursor-pointer"
+            onClick={onSubmit}
+          >
             search
           </button>
         </div>
